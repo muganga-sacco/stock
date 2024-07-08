075b5853c1cb961764f4ef12599d2266fbc3f2af
kalisakelly <kalisakelly2001@gmail.com>
2024-07-08 11:42:32 +0200

    updating fixtures

--- stock/stock/doctype/vehicle_request/vehicle_request.js
+++ stock/stock/doctype/vehicle_request/vehicle_request.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2024, mugangasacco and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Vehicle Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("Vehicle Request", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
